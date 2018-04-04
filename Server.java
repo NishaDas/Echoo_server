e6ee81b4020f93b34d6ccc3e825cf64e8074878e
nisha <nishavava1996@gmail.com>
2018-04-04 10:41:01 +0530

    input output functions are created

--- Server.java
+++ Server.java
@@ -2,3 +2,10 @@ import java.net.*;
 import java.io.*; 
 class Server
 {
+public static void main(String args[])throws Exception
+	{
+			ServerSocket ss=new ServerSocket(3333);  
+    		Socket s=ss.accept();  
+    		DataInputStream din=new DataInputStream(s.getInputStream());  
+    		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
